d743e3c6b272fa96dc160e2cf09dbb67541abcd6
Juan Carlos Bravo M. <juancarlos99f@hotmail.com>
2024-06-07 15:55:42 -0600

    loop pendiente

--- Principal.java
+++ Principal.java
@@ -8,6 +8,7 @@ public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner sc = new Scanner(System.in);
+        Scanner cantidadDeseada = new Scanner(System.in);
         ConsultaApi consulta = new ConsultaApi();
         System.out.println("""
             ---------------------------------------------
@@ -16,35 +17,45 @@ public class Principal {
                 1. USD
                 2. CAD
                 3. EUR
-                4. MXN
-
+                4. MXN 
+            
                 5. Salir
 
-                ---------------------------------------------
+            ---------------------------------------------
                 """);
             System.out.println("Introduce opcion: ");
+
+        
 
         try {
             int eleccion = Integer.parseInt(sc.nextLine());
             double valor = consulta.busquedaApi(eleccion);
             System.out.println("El valor de la opción seleccionada es: " + valor);
+
+            System.out.println("Introduzca cantidad a combertir: ");
+
+            int eleccion2 = Integer.parseInt(cantidadDeseada.nextLine());
+            System.out.println("Cantidad a cambiar " + eleccion2);
+
+            if (eleccion == 1){
+                double resultado = eleccion2 * valor;
+                System.out.println("Cantidad comvertida = " + resultado);
+            }else if (eleccion == 2){
+                double resultado = eleccion2 * valor;
+                System.out.println("Cantidad comvertida = " + resultado);
+            }else if (eleccion == 3){
+                double resultado = eleccion2 * valor;
+                System.out.println("Cantidad comvertida = " + resultado);
+            }else if (eleccion == 4){
+                double resultado = eleccion2 * valor;
+                System.out.println("Cantidad comvertida = " + resultado);
+            }
+ 
+            
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
             System.out.println("Finalizo aplicacion");
         }
-
-        // try{
-        //     var eleccion = Integer.valueOf(sc.nextLine());
-
-        //     Api api = consulta.busquedaApi(eleccion);
-        //     System.out.println(api);
-        //     //System.out.println(api);
-
-        // }catch (RuntimeException e ){
-        //     System.out.println(e.getMessage());
-        //     System.out.println("Finalizo aplicacion");
-        // }
-
 
     }
 }
