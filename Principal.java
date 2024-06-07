614734ac1a18c53bc0b57f2d66b81e1e1f997789
Juan Carlos Bravo M. <juancarlos99f@hotmail.com>
2024-06-07 13:48:02 -0600

    creacion de clases y HttpRequest

--- Principal.java
+++ Principal.java
@@ -1,8 +1,39 @@
 package CONVERSOR;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Principal {
-    public static void main(String[] args) {
-        // PROBANDO COMMIT
-        
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        ConsultaApi consulta = new ConsultaApi();
+        System.out.println("""
+                ############################################
+                Seleccione opcion deseada:
+
+                1.
+                2.
+                3.
+                4.
+
+                5.
+
+                """);
+
+        try{
+            var eleccion = Integer.valueOf(sc.nextLine());
+
+            Api api = consulta.busquedaApi(eleccion);
+            System.out.println(api);
+            //System.out.println(api);
+
+        }catch (RuntimeException e ){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizo aplicacion");
+        }
+
+
     }
 }
